7357aa1b56a500cf8a30efc01ed09207ef41d77e
Federico Resnizky <fresnizky@yahoo.com.ar>
2018-03-06 20:28:12 -0300

    Filter ingredients with passed use-by date

--- src/LunchTime/LunchController.php
+++ src/LunchTime/LunchController.php
@@ -5,12 +5,19 @@ use Silex\Application;
 use Symfony\Component\HttpFoundation\Request;
  
 class LunchController {
+  protected $currentDate;
 
   public function lunch() {
     $ingredients = json_decode(file_get_contents(__DIR__ . '/../../data/ingredients.json'), true);
     $recipes = json_decode(file_get_contents(__DIR__ . '/../../data/recipes.json'), true);
   
-    return print_r($recipes);
-  }
+    $this->currentDate = new \DateTime();
+    $ingredients = array_filter($ingredients['ingredients'], function($ingredient) {
+        $ingredientDate = \DateTime::createFromFormat('Y-m-d', $ingredient['use-by']);
 
+        return $ingredientDate > $this->currentDate;
+    });
+
+    return print_r($ingredients);
+  }
 }
